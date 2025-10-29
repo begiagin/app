2fb051ba320ff3f722e63ff6b037f4f71b6aac4e
Alireza Salehi <engalirezasalehi@gmail.com>
2025-10-29 23:05:25 +0330

    add laser.json and related code to project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.viewpager2:viewpager2:1.1.0")
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.airbnb.android:lottie:6.4.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
--- app/src/main/java/com/halata/blueapp/SplashActivity.java
+++ app/src/main/java/com/halata/blueapp/SplashActivity.java
@@ -6,13 +6,14 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.DisplayMetrics;
 import android.widget.ScrollView;
-
+import com.airbnb.lottie.LottieAnimationView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
     // Set the duration of the splash screen in milliseconds
-    private static final int SPLASH_DURATION = 3000; // 3 seconds
+    private static final int SPLASH_DURATION = 6000; // 3 seconds
     private ScrollView svMain;
+    private LottieAnimationView lottieAnimationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,9 +31,13 @@ public class SplashActivity extends AppCompatActivity {
         int[] colors = {0xFFFF0550, 0xFF0110FF}; // Red to Blue
         GradientDrawable gd = new GradientDrawable(
                 GradientDrawable.Orientation.TOP_BOTTOM, colors);
+        lottieAnimationView = findViewById(R.id.lottieView);
+        lottieAnimationView.playAnimation();
+
 
 
         svMain.setBackground(gd);
+
 
         // Use a Handler to post a delayed action for transitioning to the main activity
         new Handler().postDelayed(new Runnable() {
@@ -41,6 +46,7 @@ public class SplashActivity extends AppCompatActivity {
                 // Start the main activity after the splash screen duration
                 Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
+
                 finish();
             }
         }, SPLASH_DURATION);
